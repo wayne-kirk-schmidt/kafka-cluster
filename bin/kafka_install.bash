7546dd5338a158cd5ad3221b9c09df879f06a2a8
Wayne Kirk Schmidt <wayne.kirk.schmidt@gmail.com>
2023-11-01 10:19:16 +0900

    shell check compliant

--- bin/kafka_install.bash
+++ bin/kafka_install.bash
@@ -5,8 +5,8 @@ umask 022
 export PATH="/usr/bin:/usr/local/bin:/sbin:/usr/sbin:$PATH"
 
 ### Define the bin and the etc directory as related to the running script
-export BINDIR=$(dirname "$(realpath "$0")")
-export ETCDIR=$( realpath $BINDIR/../etc )
+BINDIR=$(dirname "$(realpath "$0")")
+ETCDIR=$( realpath "$BINDIR"/../etc )
 
 ### Define the download directory
 export DOWNLOAD_DIR="/var/tmp/downloads"
@@ -23,14 +23,14 @@ zookeeper wget apt-transport-https ca-certificates \
 curl gnupg-agent gnupg software-properties-common
 
 ### Now Prepare to install logstash
-export ELASTICKEY="/usr/share/keyrings/elastic-keyring.gpg"
-export ELASTICSRC="https://artifacts.elastic.co/packages/8.x/apt"
+ELASTICKEY="/usr/share/keyrings/elastic-keyring.gpg"
+ELASTICSRC="https://artifacts.elastic.co/packages/8.x/apt"
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
-	gpg --dearmor -o "$ELASTICKEY"
+        gpg --dearmor -o "$ELASTICKEY"
 
 echo "deb [signed-by=$ELASTICKEY] $ELASTICSRC stable main" | \
-	sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
+        sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 
 ### Now install logstash
 apt-get update -y
@@ -38,75 +38,75 @@ apt-get install -y logstash
 
 ### Prepare download directory for non debian package installations
 mkdir -p ${DOWNLOAD_DIR}
-cd ${DOWNLOAD_DIR}
+cd ${DOWNLOAD_DIR} || exit
 
 ### Retrieve the Kafka installation image
 ### wget https://downloads.apache.org/kafka/3.6.0/kafka_2.13-3.6.0.tgz
 
-export BASEURL="https://downloads.apache.org/kafka/"
+BASEURL="https://downloads.apache.org/kafka/"
 
-export KAFKA_BASE=$( curl -s ${BASEURL} | \
-        egrep -i "\[DIR\]" | grep -o -E 'href="[^"]+"' | \
+KAFKA_BASE=$( curl -s "${BASEURL}" | \
+        grep -E -i "\[DIR\]" | grep -o -E 'href="[^"]+"' | \
         cut -d'"' -f2 | sed 's/\///' | sort -rbn | head -1 )
 
-export KAFKA_FILE=$( curl -s ${BASEURL}/${KAFKA_BASE}/ | \
-        egrep -i '\[\s+\]' | egrep -iv '(src|docs).tgz' | \
+KAFKA_FILE=$( curl -s "${BASEURL}/${KAFKA_BASE}/" | \
+        grep -E -i '\[\s+\]' | grep -E -iv '(src|docs).tgz' | \
         grep -o -E 'href="[^"]+"' | cut -d'"' -f2 | sort -rbn | head -1 )
 
-rm -f ${DOWNLOAD_DIR}/${KAFKA_FILE}
+rm -f "${DOWNLOAD_DIR}/${KAFKA_FILE}"
 
-wget ${BASEURL}${KAFKA_BASE}/${KAFKA_FILE}
+wget "${BASEURL}${KAFKA_BASE}/${KAFKA_FILE}"
 
 ### Unpack and install the installation image
-export KAFKAFILE=$( ls -1d kafka*.tgz)
+KAFKAFILE=$( ls -1d kafka*.tgz)
 
-tar -xf $KAFKAFILE
+tar -xf "$KAFKAFILE"
 
-export KAFKADIR=$( basename $KAFKAFILE .tgz )
-export KAFKA_BASE_DIR="/usr/local/kafka"
-mkdir -p ${KAFKA_BASE_DIR}
+KAFKADIR=$( basename "$KAFKAFILE" .tgz )
+KAFKA_BASE_DIR="/usr/local/kafka"
+mkdir -p "${KAFKA_BASE_DIR}"
 
-export KAFKA_ETC_DIR="/etc/kafka"
-mkdir -p ${KAFKA_ETC_DIR}
+KAFKA_ETC_DIR="/etc/kafka"
+mkdir -p "${KAFKA_ETC_DIR}"
 
-export KAFKA_SVC_DIR="/etc/systemd/system"
-mkdir -p ${KAFKA_SVC_DIR}
+KAFKA_SVC_DIR="/etc/systemd/system"
+mkdir -p "${KAFKA_SVC_DIR}"
 
-export KAFKA_DATA_DIR="/var/data/kafka"
-export ZOOKEEPER_DATA_DIR="/var/data/zookeeper"
+KAFKA_DATA_DIR="/var/data/kafka"
+ZOOKEEPER_DATA_DIR="/var/data/zookeeper"
 mkdir -p "${KAFKA_DATA_DIR}" "${ZOOKEEPER_DATA_DIR}"
 
-export KAFKA_LOGS_DIR="/var/log/kafka"
-export ZOOKEEPER_LOGS_DIR="/var/log/zookeeper"
+KAFKA_LOGS_DIR="/var/log/kafka"
+ZOOKEEPER_LOGS_DIR="/var/log/zookeeper"
 mkdir -p "${KAFKA_LOGS_DIR}" "${ZOOKEEPER_LOGS_DIR}"
 
-mv $KAFKADIR/* ${KAFKA_BASE_DIR}
+mv "$KAFKADIR"/* "${KAFKA_BASE_DIR}"
 
-touch ${KAFKA_BASE_DIR}/$KAFKADIR
+touch "${KAFKA_BASE_DIR}/$KAFKADIR"
 
-rm -f ${DOWNLOAD_DIR}/${KAFKA_FILE}
+rm -f "${DOWNLOAD_DIR}/${KAFKA_FILE}"
 
 ### Setup the appropriate variables for the properties files
-export SRC_KAFKA_CFG="$ETCDIR/server.properties"
-export SRC_ZOOKEEPER_CFG="$ETCDIR/zookeeper.properties"
+SRC_KAFKA_CFG="$ETCDIR/server.properties"
+SRC_ZOOKEEPER_CFG="$ETCDIR/zookeeper.properties"
 
-export DST_KAFKA_CFG="$KAFKA_ETC_DIR/server.properties"
-export DST_ZOOKEEPER_CFG="$KAFKA_ETC_DIR/zookeeper.properties"
+DST_KAFKA_CFG="$KAFKA_ETC_DIR/server.properties"
+DST_ZOOKEEPER_CFG="$KAFKA_ETC_DIR/zookeeper.properties"
 
 ### Install the appropriate configuration files
-cp ${SRC_KAFKA_CFG} ${DST_KAFKA_CFG}
-cp ${SRC_ZOOKEEPER_CFG} ${DST_ZOOKEEPER_CFG}
+cp "${SRC_KAFKA_CFG}" "${DST_KAFKA_CFG}"
+cp "${SRC_ZOOKEEPER_CFG}" "${DST_ZOOKEEPER_CFG}"
 
 ### Setup the appropriate variables for the Service definition files
-export SRC_KAFKA_SVC="$ETCDIR/kafka.service"
-export SRC_ZOOKEEPER_SVC="$ETCDIR/zookeeper.service"
+SRC_KAFKA_SVC="$ETCDIR/kafka.service"
+SRC_ZOOKEEPER_SVC="$ETCDIR/zookeeper.service"
 
-export DST_KAFKA_SVC="$KAFKA_SVC_DIR/kafka.service"
-export DST_ZOOKEEPER_SVC="$KAFKA_SVC_DIR/zookeeper.service"
+DST_KAFKA_SVC="$KAFKA_SVC_DIR/kafka.service"
+DST_ZOOKEEPER_SVC="$KAFKA_SVC_DIR/zookeeper.service"
 
 ### Install the appropriate service definition files
-cp ${SRC_KAFKA_SVC} ${DST_KAFKA_SVC}
-cp ${SRC_ZOOKEEPER_SVC} ${DST_ZOOKEEPER_SVC}
+cp "${SRC_KAFKA_SVC}" "${DST_KAFKA_SVC}"
+cp "${SRC_ZOOKEEPER_SVC}" "${DST_ZOOKEEPER_SVC}"
 
 ### Now start the kafka service
 systemctl start kafka
